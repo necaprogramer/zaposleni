6affa1b3daeb9984170bb82fbc9056de4796e291
necaprogramer <nissimbara@outlook.com>
2023-01-15 13:26:21 +0100

    Change json to include additional data, add functionality to change json upon verified vacation request

--- zahtev-poslat.php
+++ zahtev-poslat.php
@@ -4,4 +4,22 @@ $vremeOd = htmlspecialchars($_POST['vreme-od']);
 $vremeDo = htmlspecialchars($_POST['vreme-do']);
 
 echo $imePrezime . ", uspesno ste podneli zahtev administratoru za odmor u periodu od " . $vremeOd . " do " . $vremeDo;
+
+$filename = 'zaposleni.json';
+if(file_exists($filename)){
+    $data = file_get_contents($filename);
+    $zaposleni = json_decode($data);
+    
+    foreach($zaposleni as $tim=>$radnici){
+        foreach($radnici as $radnik){
+            if($radnik->imePrezime == $imePrezime){
+                $radnik->zahtevaniOdmor->od = $vremeOd;
+                $radnik->zahtevaniOdmor->do = $vremeDo;
+            }
+        }
+    };
+    
+    $zahtevi = json_encode($zaposleni);
+    file_put_contents($filename, $zahtevi);
+}
 ?>
